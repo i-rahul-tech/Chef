1c391369182c88a7638e0e9c413c7d9019c388ff
root <root@ip-172-31-37-163.us-east-2.compute.internal>
2019-09-24 11:40:15 +0000

    Added Server.rb

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,4 +2,17 @@
 # Cookbook:: apache
 # Recipe:: server
 #
-# Copyright:: 2019, The Authors, All Rights Reserved.
+# Copyright:: 2020, The Authors, All Rights Reserved.
+#
+
+package 'apache2' do
+	action :install
+end
+
+file 'var/www/html/index.html' do
+	content '<h1>Hello</h1>'
+end
+
+service 'apache2' do
+	action [:enable,:start]
+end
